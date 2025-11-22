008d763aee8e6e852b5c3e94c0b7d7c23607722c
Chris Allison <chrisallisonsax@gmail.com>
2025-11-21 22:46:38 -0500

    Fixed lab component

--- app/Labs/Lab5/WorkingWithObjectsAsynchronously.tsx
+++ app/Labs/Lab5/WorkingWithObjectsAsynchronously.tsx
@@ -11,7 +11,12 @@ interface Assignment {
 }
 
 export default function WorkingWithObjectsAsynchronously() {
-    const [assignment, setAssignment] = useState<Assignment>({});
+    const [assignment, setAssignment] = useState<Assignment>({
+        title: "",
+        description: "",
+        due: "",
+        completed: false,
+    });
     const fetchAssignment = async () => {
         const assignment = await client.fetchAssignment();
         setAssignment(assignment);
@@ -28,19 +33,47 @@ export default function WorkingWithObjectsAsynchronously() {
         <div id="wd-asynchronous-objects">
             <h3>Working with Objects Asynchronously</h3>
             <h4>Assignment</h4>
-            <FormControl defaultValue={assignment.title} className="mb-2"
-                onChange={(e) => setAssignment({ ...assignment, title: e.target.value })} />
-            <FormControl as="textarea" rows={3} defaultValue={assignment.description} className="mb-2"
-                onChange={(e) => setAssignment({ ...assignment, description: e.target.value })} />
-            <FormControl type="date" className="mb-2" defaultValue={assignment.due}
-                onChange={(e) => setAssignment({ ...assignment, due: e.target.value })} />
+            <FormControl
+                defaultValue={assignment.title}
+                className="mb-2"
+                onChange={(e) =>
+                    setAssignment({ ...assignment, title: e.target.value })
+                }
+            />
+            <FormControl
+                as="textarea"
+                rows={3}
+                defaultValue={assignment.description}
+                className="mb-2"
+                onChange={(e) =>
+                    setAssignment({ ...assignment, description: e.target.value })
+                }
+            />
+            <FormControl
+                type="date"
+                className="mb-2"
+                defaultValue={assignment.due}
+                onChange={(e) => setAssignment({ ...assignment, due: e.target.value })}
+            />
             <div className="form-check form-switch">
-                <input className="form-check-input" type="checkbox" id="wd-completed"
+                <input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="wd-completed"
                     checked={assignment.completed || false}
-                    onChange={(e) => setAssignment({ ...assignment, completed: e.target.checked })} />
-                <label className="form-check-label" htmlFor="wd-completed"> Completed </label>
+                    onChange={(e) =>
+                        setAssignment({ ...assignment, completed: e.target.checked })
+                    }
+                />
+                <label className="form-check-label" htmlFor="wd-completed">
+                    {" "}
+                    Completed{" "}
+                </label>
             </div>
-            <button className="btn btn-primary me-2" onClick={() => updateTitle(assignment.title)} >
+            <button
+                className="btn btn-primary me-2"
+                onClick={() => updateTitle(assignment.title)}
+            >
                 Update Title
             </button>
 
@@ -49,4 +82,3 @@ export default function WorkingWithObjectsAsynchronously() {
         </div>
     );
 }
-
